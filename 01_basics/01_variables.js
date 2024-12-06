422f60e5e36a75515fcc2f8b3f70a61bca4fbb06
arnavj8 <arnavj096@gmail.com>
2024-12-06 15:59:53 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail ="arnav@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 //not allowed
+
+
+accountEmail="hc@hc.com"
+accountPassword ="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var because of issue in block scope and fucntional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
